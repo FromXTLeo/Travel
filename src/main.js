5c260144ec34d79ea0307205b71b5547f2fd2420
leid <164632314@qq.com>
2018-05-09 10:58:33 +0800

    项目预热

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
+import fastclick from 'fastclick'
+//解决某些机型click事件延迟bug
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
